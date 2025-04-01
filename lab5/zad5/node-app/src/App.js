91e1e3fd91ec568abe415fb9f33b5890c781b29f
Maciej Rybarczyk <maciu400@gmail.com>
2025-04-02 01:28:15 +0200

    Zadanie 5

---
+++ lab5/zad5/node-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Docker Multi-Stage Build Demo</h1>
+        <p>This React app was built in a multi-stage Docker build process.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
